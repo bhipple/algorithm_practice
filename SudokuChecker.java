1f13811de7faa37a92e6e3a2574d3f320ce9a706
brhCS <benjamin.hipple@gmail.com>
2013-06-03 22:38:23 -0300

    Create SudokuChecker.java

---
+++ SudokuChecker.java
@@ -0,0 +1,53 @@
+public class SudokuChecker {
+	
+	public static boolean isValid(int[][] sudoku) {
+		boolean[] checker;
+
+		// check rows
+		for(int i=0; i<9; i++) {
+			checker = new boolean[10];
+			for(int j=0; j<9; j++) {
+				if(checker[(sudoku[i][j])]) {
+					return false;
+				}
+				checker[(sudoku[i][j])] = true;
+			}
+		}
+		
+		// check columns
+		for(int j=0; j<9; j++) {
+			checker = new boolean[10];
+			for(int i=0; i<9; i++) {
+				if(checker[(sudoku[i][j])]) {
+					return false;
+				}
+				checker[(sudoku[i][j])] = true;
+			}
+		}
+
+		// check boxes
+		for(int i=0; i<6; i+=3) {
+			for(int j=0; j<6; j+=3) {
+				if(!checkBox(sudoku, i, j))
+					return false;
+			}
+		}
+
+		
+		return true;
+	}
+	
+	private static boolean checkBox(int[][] sudoku, int x, int y) {
+		boolean[] checker = new boolean[10];
+		
+		for(int i=x; i<x+3; i++) {
+			for(int j=y; j<y+3; j++) {
+				if(checker[(sudoku[i][j])])
+					return false;
+				checker[sudoku[i][j]] = true;
+			}
+		}
+		return true;
+	}
+
+}
