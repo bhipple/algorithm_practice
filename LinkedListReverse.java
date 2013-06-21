6779e9e4fd4d3b2d8be60e325aa877a2e4014379
brhCS <benjamin.hipple@gmail.com>
2013-06-21 11:26:55 -0600

    Update LinkedListReverse.java

--- LinkedListReverse.java
+++ LinkedListReverse.java
@@ -4,16 +4,6 @@
  *
  */
 public class LinkedListReverse {
-
-  public static void main(String[] args) {
-		ListNode a = new ListNode(5);
-		a.next = new ListNode(6);
-		a.next.next = new ListNode(7);
-		a.next.next.next = new ListNode(8);
-		
-		System.out.println(reverse(a).next.next.next.data);
-	}
-	
 	public static ListNode reverse(ListNode a) {	
 		ListNode b = a.next;
 		a.next = null;
