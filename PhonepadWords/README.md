1b1ea91aba47842831b674734a28b0bd4c3758d3
bhipple <bhipple@bloomberg.net>
2015-01-06 19:08:07 -0500

    Phonepad

---
+++ PhonepadWords/README.md
@@ -0,0 +1,1 @@
+Print out all phonepad alphabet sequences that could be represented by an integer with digits in [2-9]  
---
+++ PhonepadWords/possible_words.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+keyToLetters = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f'],
+    ['g', 'h', 'i'],
+    ['j', 'k', 'l'],
+    ['m', 'n', 'o'],
+    ['p', 'q', 'r', 's'],
+    ['t', 'u', 'v'],
+    ['w', 'x', 'y', 'z']
+]
+
+class Node:
+    def __init__(self, let):
+        self.letter = let
+        self.children = []
+
+def intToListOfNodes(x):
+    res = []
+
+    for letter in keyToLetters[x - 2]:
+        res.append(Node(letter))
+
+    return res
+
+def phoneWords(head, x):
+    if not len(x) > 0:
+        return None
+
+    head.children = intToListOfNodes(int(x[0]))
+
+    if not head.children == None:
+        for child in head.children:
+            phoneWords(child, x[1:])
+
+
+# Testing
+def printPath(head, sofar):
+    if head == None:
+        return
+    if head.children == None or not len(head.children):
+        print sofar + head.letter
+        return
+
+    for child in head.children:
+        printPath(child, sofar + head.letter)
+
+
+#x = input("Enter a phonepad sequence: ")
+x = 259;
+
+head = Node(' ')
+phoneWords(head, str(x))
+printPath(head, "")
