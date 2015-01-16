b594243fd808a5a3e0eee974bfda91238c8a0a1b
Benjamin Hipple <benjamin.hipple@gmail.com>
2015-01-15 22:14:41 -0500

    Create triangle_dfs.py

---
+++ TrianglePathSum/triangle_dfs.py
@@ -0,0 +1,10 @@
+// O(2^(L-2)) runtime, where L is the depth of the DAG
+def dfs(i, rowNum):
+	if rowNum >= len(rows):
+		return 0
+	return rows[rowNum][i] + max(dfs(i, rowNum + 1), dfs(i + 1, rowNum + 1))
+	
+
+# Test
+rows = [[5], [9, 6], [4, 6, 8], [0, 7, 1, 5]]
+print str(dfs(0, 0))
