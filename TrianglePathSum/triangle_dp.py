c2a5d95218e1c24498bdd117bb6e4b042be0d561
bhipple <bhipple@bloomberg.net>
2015-01-16 22:35:45 -0500

    Finished dynamic programming approach to triangle weight

--- TrianglePathSum/triangle_dp.py
+++ TrianglePathSum/triangle_dp.py
@@ -7,21 +7,29 @@ def printTriangle(rows):
             thisRow += " " + str(rows[i][j])
         print thisRow
 
-
+# O(row count) runtime
 def dp(rows):
     costs = [0] * len(rows)
     for i in range(len(rows)):
         costs[i] = [0] * len(rows[i])
 
     for j in range(len(rows[len(rows)-1])):
-        costs[i][j] = rows[i][j]
+        costs[i][j] = int(rows[i][j])
 
     for i in range(len(rows) - 2, -1, -1):
         for j in range(len(rows[i])):
-            costs[i][j] = rows[i][j] + max(costs[i+1][j], costs[i+1][j+1])
+            costs[i][j] = int(rows[i][j]) + max(costs[i+1][j], costs[i+1][j+1])
 
     return costs[0][0]
 
 # Test
 rows = [[5], [9, 6], [4, 6, 8], [0, 7, 1, 5]]
+print str(dp(rows))
+
+rows = []
+fh = open('triangle.txt')
+for row in fh:
+    rows.append(row.split())
+printTriangle(rows)
+
 print str(dp(rows))
