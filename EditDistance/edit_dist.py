5716067db307cb9234965e9d3a45f051bb9f0c99
bhipple <bhipple@bloomberg.net>
2015-01-16 22:05:06 -0500

    Memo might be working?

--- EditDistance/edit_dist.py
+++ EditDistance/edit_dist.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python
-
-
 # Naive implementation
 def editDist(a, b, dist):
     if a == b:
@@ -15,10 +13,10 @@ def editDist(a, b, dist):
     return min(editDist(a[1:], b, dist + 1), editDist(a, b[1:], dist + 1))
 
 
-memo = dict()
-
+# Dynamic Programming
 def hash(a, b):
     return a + "_" + b
+memo = {}
 
 def editDist_dp(a, b, dist):
     if a == b:
@@ -31,16 +29,22 @@ def editDist_dp(a, b, dist):
         memo[hash(a,b)] = dist + len(a)
         return dist + len(a)
     elif a[0] == b[0]:
-        return editDist_dp(a[1:], b[1:], dist)
-
-    if hash(a, b) in memo:
-        print "used the memo for %s, %s" % (a, b)
+        memo[hash(a,b)] = editDist_dp(a[1:], b[1:], dist)
         return memo[hash(a,b)]
 
-    return min(editDist_dp(a[1:], b, dist + 1), editDist_dp(a, b[1:], dist + 1))
+    if hash(a, b) in memo:
+        #print "used the memo for %s, %s = %d" % (a, b, memo[hash(a,b)])
+        return memo[hash(a,b)]
 
+    memo[hash(a,b)] = min(editDist_dp(a[1:], b, dist + 1), editDist_dp(a, b[1:], dist + 1))
+    return memo[hash(a,b)]
+
+
+# Testing
 print "ab - a = %d (expecting 1)" % editDist_dp("ab", "a", 0)
 print "ab - ca = %d (expecting 2)" % editDist_dp("ab", "ca", 0)
 print "abc - bca = %d (expecting 2)" % editDist_dp("abc", "bca", 0)
 print "abcd - bca = %d (expecting 3)" % editDist_dp("abcd", "bca", 0)
-#print editDist("asdghasefhaseca", "ahgasddgslskgjaddlkjsdlkgjadfgkljb", 0)
+print "asdfasdf - asdf = %d (expecting 4)" % editDist_dp("asdfasdf", "asdf", 0)
+print "asdfasdf - aaf = %d (expecting 5)" % editDist_dp("asdfasdf", "aaf", 0)
+print editDist("asdghaca", "ahgakgjadfgkljb", 0)
