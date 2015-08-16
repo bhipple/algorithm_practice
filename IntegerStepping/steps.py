0db7afa6465e8ad75dbf3f727f97487783d8ae33
Benjamin Hipple <brh2113@columbia.edu>
2015-08-16 05:37:22 -0400

    simpler solution, with Inna's help

--- IntegerStepping/steps.py
+++ IntegerStepping/steps.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # Naive solution that works with suboptimal results
+MAX_INT = 2000000000
+
 def steps(X):
     n = 1
     while n*(n+1)/2 <= X:
@@ -15,8 +17,9 @@ def maze(source, step, destination, upperLim):
         return step
     if step > upperLim:
         return -1
-    plus = maze(source+step+1, step+1, destination, upperLim)
-    minus = maze(source-step-1, step+1, destination, upperLim)
+    step += 1
+    plus = maze(source+step, step, destination, upperLim)
+    minus = maze(source-step, step, destination, upperLim)
 
     if plus > 0:
         if minus > 0:
@@ -29,6 +32,19 @@ def maze(source, step, destination, upperLim):
 def combined(destination):
     upperLim = steps(destination)
     return maze(0, 0, destination, upperLim)
+
+# This uses the intution that it should not be necessary to
+# go past the destination and backtrack, and that provides
+# the recursive base case.
+def good(source, step, destination):
+    if source == destination:
+        return step
+    if abs(source) > destination:
+        return MAX_INT
+    step += 1
+    plus = good(source+step, step, destination)
+    minus = good(source-step, step, destination)
+    return min(plus, minus)
 
 for i in range(22):
     print "It takes %s steps to get to %s" % (combined(i), i)
