1c49fd31ce4825cea1a5a2633cee97990e276cff
Benjamin Hipple <brh2113@columbia.edu>
2015-11-15 14:01:21 -0500

    Some haskell warmup exercises

---
+++ power_sum.hs
@@ -0,0 +1,21 @@
+-- Given x and n, calculate the number of ways to express
+-- x as the sum of y_i^n for i in [1..]
+
+powerset :: [Int] -> Int -> [[Int]]
+powerset [] _ = [[]]
+powerset (x:xs) t = xss ++ map (x:) xss
+    where xss = filter (\y -> sum y <= t) (powerset xs t)
+
+powerct :: Int -> Int -> Int
+powerct x n = length ways
+    where
+        powers = [k^n | k <- [1..]]
+        pows = takeWhile (\k -> k <= x) powers
+        pset = powerset pows x
+        ways = filter (\s -> (sum s == x)) pset
+
+main :: IO ()
+main = do
+        x <- getLine
+        n <- getLine
+        print $ powerct (read x :: Int) (read n :: Int)
