9a371127407276507ed3bc3ae4362f3b62aa4bf2
Benjamin Hipple <brh2113@columbia.edu>
2015-11-30 23:49:11 -0500

    Missing numbers

--- missing_numbers.hs
+++ missing_numbers.hs
@@ -1,25 +1,14 @@
 -- Given two lists A and B, find the set of numbers
 -- that appear more frequently in B
 import Data.List
-import Data.Map
+freqPair xs = map (\x -> (head x, length x)) . group . sort $ xs
 
-minVal xs ys = min (minimum xs) (minimum ys)
-
-buildMap hasher xs = fromList freqCnt
-    where freqCnt = Data.List.map (\x -> (hasher $ head x, length x)) . group . sort $ xs
-
+differ :: Eq a => [(a,a)] -> [(a,a)] -> [a]
 differ _ [] = []
-differ xMap (y:ys)
-    | Data.Map.lookup k xMap == v = differ xMap ys
-    | otherwise = v : differ xMap ys
-    where k = fst y
-          v = snd y
+differ [] (y:ys) = (fst y) : differ [] ys
+differ (x:xs) (y:ys)
+    | ((fst x) == (fst y)) && ((snd x) == (snd y)) = differ xs ys
+    | fst x == fst y = (fst y) : differ xs ys
+    | otherwise = (fst y) : differ (x:xs) ys
 
-diff :: (Num a, Ord a) => [a] -> [a] -> Map a Int
-diff xs ys = differ xMap (toList yMap)
-    where minV = minVal xs ys
-          hasher = flip (-) minV
-          unhasher = (+) minV
-          builder = buildMap hasher
-          xMap = builder xs
-          yMap = builder ys
+diff xs ys = differ (freqPair xs) (freqPair ys)
