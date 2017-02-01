32b4a8d78f77dc509f47a6a792a5373c437a9a78
Benjamin Hipple <bhipple@bloomberg.net>
2017-01-31 22:00:39 -0500

    PLaying with move semantics and copy elision

--- language/move-semantics/t.cpp
+++ language/move-semantics/t.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <stdlib.h>
 
+// See slide 28 of this presentation for an overview of which special members
+// are or are not provided by the compiler, depending on which special members
+// the user declares.
+// https://howardhinnant.github.io/bloomberg_2016.pdf
 class Foo {
   public:
     Foo() {
@@ -10,17 +14,40 @@ class Foo {
         y = std::to_string(x);
     }
 
+    Foo(const Foo& other): x(other.x), y(other.y) {
+        std::cout << "Copied a Foo " << std::endl;
+    }
+
+    Foo(Foo&& other) noexcept: x(other.x), y(other.y) {
+        std::cout << "Moved a Foo " << std::endl;
+    }
+
+    Foo& operator=(const Foo& other) {
+        std::cout << "operator= copy a Foo" << std::endl;
+        Foo tmp(other);
+        *this = std::move(tmp);
+        return *this;
+    }
+
+    Foo& operator=(const Foo&& other) noexcept {
+        std::cout << "operator= move a Foo" << std::endl;
+        x = other.x;
+        y = other.y;
+        return *this;
+    }
+
     int x;
     std::string y;
 };
 
-std::vector<Foo> getFoos(int ct) {
+std::vector<Foo> getFoos(size_t ct) {
     std::vector<Foo> vf(ct);
     return vf;
 }
 
-std::vector<Foo> movingFoos(int ct) {
+std::vector<Foo> movingFoos(size_t ct) {
     std::vector<Foo> vf;
+    vf.reserve(ct);
     for(size_t i = 0; i < ct; ++i) {
         Foo f;
         vf.push_back(std::move(f));
@@ -31,16 +58,18 @@ std::vector<Foo> movingFoos(int ct) {
 int main() {
     Foo f;
 
+    std::cout << "\n****************************************\n\n";
     Foo g = std::move(f);
-    std::cout << "Moved f to g with x = " << g.x << " at " << &g << std::endl;
+    std::cout << "^ Moved f to g with x = " << g.x << " at " << &g << std::endl;
 
     Foo h = f;
-    std::cout << "Valid but unspecified state: h.x from f: " << h.x << std::endl;
+    std::cout << "^ Valid but unspecified state: h.x from f: " << h.x << std::endl;
 
-
+    std::cout << "\n****************************************\n\n";
     std::vector<Foo> vf = getFoos(5);
     std::cout << "^ Vector of 5 Foos returned from function at " << &vf << std::endl;
 
+    std::cout << "\n****************************************\n\n";
     std::vector<Foo> vf2 = movingFoos(5);
     std::cout << "^ Vector from movingFoos at " << &vf2 << std::endl;
 }
